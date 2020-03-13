2c88afeeb52ef92ba72e07e55ff2260fd6f684bb
Xulio-Xulio <61099971+Xulio-Xulio@users.noreply.github.com>
2020-03-13 00:46:01 -0300

    Hello again

--- site_links/script.js
+++ site_links/script.js
@@ -3,11 +3,11 @@ function click() {
     var email = document.querySelector(`div#email`)
     var error = document.querySelector(`div#error`)
     error.innerHTML = `Please enter an email address`
-    //if( email == `NULL`){
-    //    error.innerHTML = `Please enter an email address`
-   // }
-    //else{
-//
-    //}
+    if( email == `NULL`){
+        error.innerHTML = `Please enter an email address`
+    }
+    else{
+
+    }
 
 }
